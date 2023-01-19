bcf89508ad1805f2954a78a45a57d5dc17590a8a
KaiChengDM <101462680+KaiChengDM@users.noreply.github.com>
2023-01-19 14:21:04 +0100

    Add files via upload

--- M5.m
+++ M5.m
@@ -39,7 +39,7 @@ hyperpar.lb = 5*10^-4.*ones(1,n);
 hyperpar.ub = 10*ones(1,n);
 hyperpar.corr_fun = 'corrbiquadspline';
 %hyperpar.corr_fun = 'corrspline';
-% hyperpar.corr_fun = 'corrgaussian';
+%  hyperpar.corr_fun = 'corrgaussian';
 hyperpar.opt_algorithm = 'Hooke-Jeeves';
 % hyperpar.opt_algorithm = 'Fmincon';
 hyperpar.multistarts = 10;
@@ -54,67 +54,30 @@ t1=clock;
  GEKriging_Model = GEKriging_fit(inputpar,hyperpar);
 t2=clock;
 
-%% Training Sliced GE-Kriging
+Time2 = etime(t2,t1)
+[Mean Variance] = GEKriging_predictor(xtest,GEKriging_Model);
+MSE = mean((Mean-y1).^2)/var(y1)
 
+
+%% Kriging
+
+hyperpar.theta = 0.1.*ones(1,n); 
+hyperpar.lb = 5*10^-4.*ones(1,n);
+hyperpar.ub = 5*ones(1,n);
 % hyperpar.corr_fun = 'corrbiquadspline';
- hyperpar.corr_fun = 'corrspline';
-%  hyperpar.corr_fun = 'corrgaussian';
+hyperpar.corr_fun = 'corrgaussian';
 hyperpar.opt_algorithm = 'Hooke-Jeeves';
+hyperpar.opt_algorithm ='CMAES';
 hyperpar.multistarts = 10;
-inputpar.threshold = 1;
+
 inputpar.x = x;
 inputpar.y = y;
-inputpar.grad = grad_y;
-inputpar.lb = lb;
-inputpar.ub = ub;
-inputpar.snum = 10;
 
-t1 = clock;
-  GEKriging_Model2 = GEKriging_fit21(inputpar,hyperpar);
-t2 = clock;
-t1 = clock;
-  GEKriging_Model3 = GEKriging_fit31(inputpar,hyperpar);
-t2 = clock;
+t1=clock;
+  Kriging_Model = Kriging_fit(inputpar,hyperpar);
+t2=clock;
 
-Time2 = etime(t2,t1)
-[Mean Variance] = GEKriging_predictor(xtest,GEKriging_Model2);
-MSE2 = mean((Mean-y1).^2)/var(y1)
-
-
- %% Response 
-
- nn = 10000;
- xx = lb:(ub-lb)/(nn-1):ub;
- 
- %plot(xx,g(xx'),'linewidth',1.5); hold on
- %plot(x,y,'ro','linewidth',1.5); hold on
-
-[y1 v1] = GEKriging_predictor(xx',GEKriging_Model);
-[y2 v2] = GEKriging_predictor(xx',GEKriging_Model2);
-[y3 v3] = GEKriging_predictor(xx',GEKriging_Model3);
-
-  subplot(1,3,1)
-  plot(x,y,'ro','linewidth',2); hold on
-  plot(xx,g(xx'),'b-','linewidth',2); hold on
-  plot(xx,y1,'m-.','linewidth',2); hold on
-  plot(xx,sqrt(v1),'Color',[0.5 0.1 0.8],'linewidth',2); hold on
-  legend('Samples','True function','2-appendant SGE-Kriging mean','2-appendant SGE-Kriging variance')
-  xlabel('x'); ylabel('y'); 
-
-  subplot(1,3,2)
-  plot(x,y,'ro','linewidth',2); hold on
-  plot(xx,g(xx'),'b-','linewidth',2); hold on
-  plot(xx,y2,'m-.','linewidth',2); hold on
-  plot(xx,v2,'Color',[0.5 0.1 0.8],'linewidth',2); hold on
-  legend('Samples','True function','2-appendant SGE-Kriging mean','2-appendant SGE-Kriging variance')
-  xlabel('x'); ylabel('y'); 
-  
-  subplot(1,3,3)
-  plot(x,y,'ro','linewidth',2); hold on
-  plot(xx,g(xx'),'b-','linewidth',2); hold on
-  plot(xx,y3,'m-.','linewidth',2); hold on
-  plot(xx,v3,'Color',[0.5 0.1 0.8],'linewidth',2); hold on
-  legend('Samples','True function','3-appendant SGE-Kriging mean','3-appendant SGE-Kriging variance')
-  xlabel('x'); ylabel('y'); 
-
+Time1 = etime(t2,t1)
+[Mean Variance Covariance] = Kriging_predictor(xtest,Kriging_Model);
+MSE1  = mean((Mean-y1).^2)/var(y1)
 
